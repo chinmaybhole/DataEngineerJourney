ca38a149c1ecb225e75fb3abd35553cb1563fe26
Chinmay BH <chinmaybh@Chinmays-MacBook-Air.local>
2025-11-09 00:50:37 +0530

    Day 2 : Python Practice and playing with gitignore

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("hadifariborzi/amazon-books-dataset-20k-books-727k-reviews")
+# path = kagglehub.dataset_download("hadifariborzi/amazon-books-dataset-20k-books-727k-reviews")
 
-print("Path to dataset files:", path)
+# print("Path to dataset files:", path)
 
